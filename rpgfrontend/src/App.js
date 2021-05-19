2e9232c19d6d882eb9974d78e3d8c4251f69463a
CaioNunes <caiofelipe147@gmail.com>
2021-05-18 22:05:40 -0300

    Criação do projeto

---
+++ rpgfrontend/src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    cidades: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/cidades');
+    const body = await response.json();
+    this.setState({cidades: body});
+  }
+
+  render() {
+    const {cidades} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>Cidades</h2>
+            {cidades.map(cidade =>
+              <div key={cidade.id}>
+                {cidade.nome} - {cidade.tesouro}
+              </div>  
+            )}
+          </div>
+        </header>
+      </div>
+    )
+  }
+
+}
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+} */
+
+export default App;
